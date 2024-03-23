b9d4c71ccf8efafb73c5f6c3a41de02efa221104
HugoDarthenay <hugo.darthenay@gmail.com>
2024-03-23 22:22:22 +0100

    Accueil

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,57 @@
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {RouterOutlet} from "@angular/router";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+@Component({
+  selector: 'app-accueil',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,HttpClientModule],
+  templateUrl: './accueil.component.html',
+  styleUrl: './accueil.component.css'
+})
+export class AccueilComponent implements OnInit{
+  listMessage : Message[] = [];
+  index: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchMessage();
+  }
+
+  fetchMessage() {
+    this.http.get<any[]>('http://localhost:3000/messages')
+      .subscribe((data: any[]) => {
+        data.forEach(item => {
+          this.listMessage.push(new Message(item.id, item.date, item.text, new Compte(item.idCompte, item.nom, item.prenom)));
+        });
+      });
+  }
+}
+
+class Message{
+  id:number;
+  date: string;
+  text: string;
+  compte:Compte;
+
+  constructor(id: number, date: string, text: string, compte: Compte) {
+    this.id = id;
+    this.date = date;
+    this.text = text;
+    this.compte = compte;
+  }
+}
+
+class Compte{
+  id:number;
+  nom:string;
+  prenom:string;
+
+  constructor(id: number, nom: string, prenom: string) {
+    this.id = id;
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+}
