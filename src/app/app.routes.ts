411e4b4c96f8b0ac09f9200b4d5d03e11b45fae4
Hassaan-Is <is.hassaan@gmail.com>
2024-03-26 14:13:14 +0100

    ajout message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { UserComponent } from './user/user.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { SessionVerif } from './services/session-verif';
 import { ParametresComponent } from './parametres/parametres.component';
+import { MessageComponent } from './message/message.component';
 
 export const routes: Routes = [
     { path: 'login', title: 'login', component: LoginComponent },
     { path: 'signup', title: 'signup', component: SignupComponent },
     { path: 'accueil', title: 'accueil', component: AccueilComponent },
+    { path: 'message', title: 'message', component: MessageComponent },
     { path: 'user/:id', component: UserComponent },
     { path: 'parametres/:id', component: ParametresComponent },
     { path: 'user', component: UserComponent, canActivate: [SessionVerif] },
---
+++ src/app/message/message.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SessionService } from '../services/session.service';
+
+@Component({
+  selector: 'signup-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,HttpClientModule,FormsModule],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css'
+})
+
+export class MessageComponent implements OnInit{
+  formData = {
+    titre: '',
+    text :'',
+    idCompte:this.sessionService.getId()
+  };
+
+  messageCreated = false;
+
+  constructor(private http: HttpClient,private sessionService: SessionService, private router: Router) {}
+
+  ngOnInit(): void {
+    if(!this.sessionService.sessionExists()){
+      this.router.navigate(['/accueil']);
+    }
+  }
+  createMessage() {
+  this.http.post<any>('http://localhost:3000/addMessage', this.formData)
+    .subscribe(response => {
+      // Gérer la réponse du serveur ici, par exemple, afficher un message de succès
+      console.log('Compte créé avec succès :', response);
+      this.messageCreated = true;
+      setTimeout(() => {
+        this.router.navigate(['/user']); // Rediriger vers la page "login" après un délai de 2 secondes
+      }, 1000); 
+    }, error => {
+      // Gérer les erreurs ici, par exemple, afficher un message d'erreur
+      console.error('Erreur lors de la création du compte :', error);
+    });
+}}
